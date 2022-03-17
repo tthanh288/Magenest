179494cb043801c36521faa759fc0ba3a8d2fbc4
thanh <you@example.com>
2022-03-17 17:23:21 +0700

    Customize in Adminhtml

---
+++ Movie/Setup/Patch/Data/AddProductSection.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Magenest\Movie\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddProductSection implements DataPatchInterface
+{
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * AddProductAttribute constructor.
+     *
+     * @param ModuleDataSetupInterface  $moduleDataSetup
+     * @param EavSetupFactory           $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute('catalog_product', 'collapsible', [
+            'type' => 'varchar',
+            'label' => 'Collapsible label',
+            'input' => 'text',
+            'required' => false,
+            'visible_on_front' => true,
+            'apply_to' => 'simple,configurable,virtual,bundle,downloadable',
+            'unique' => false,
+            'group' => 'Collapsible',
+            'sort_order' => 1,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
